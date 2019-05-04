cd88a3ec8a2228521df54a1838074342596cd7eb
Preeternal <preeternal@ya.ru>
2019-05-04 21:55:54 +0300

    experise token

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import getUserId from "../utils/getUserId";
+import generateToken from "../utils/generateToken";
 
 const Mutation = {
   async createUser(parent, args, { prisma }, info) {
@@ -20,7 +20,7 @@ const Mutation = {
     });
     return {
       user,
-      token: jwt.sign({ userId: user.id }, "thisIsASecret")
+      token: generateToken(user.id)
     };
   },
   async login(parent, args, { prisma }, info) {
@@ -38,7 +38,7 @@ const Mutation = {
     }
     return {
       user,
-      token: jwt.sign({ userId: user.id }, "thisIsASecret")
+      token: generateToken(user.id)
     };
   },
   async deleteUser(parent, args, { prisma, request }, info) {
