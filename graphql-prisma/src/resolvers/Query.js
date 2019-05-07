fb3888c882f3070d1af0b3f96f5cccebd007cacd
Preeternal <preeternal@ya.ru>
2019-05-07 10:14:42 +0300

    sort orderBy

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -5,7 +5,8 @@ const Query = {
     const opArgs = {
       first: args.first,
       skip: args.skip,
-      after: args.after
+      after: args.after,
+      orderBy: args.orderBy
     };
     if (args.query) {
       opArgs.where = {
@@ -25,6 +26,7 @@ const Query = {
       first: args.first,
       skip: args.skip,
       after: args.after,
+      orderBy: args.orderBy,
       where: {
         author: {
           id: userId
@@ -48,6 +50,7 @@ const Query = {
       first: args.first,
       skip: args.skip,
       after: args.after,
+      orderBy: args.orderBy,
       where: {
         published: true
       }
@@ -68,7 +71,8 @@ const Query = {
     const opArgs = {
       first: args.first,
       skip: args.skip,
-      after: args.after
+      after: args.after,
+      orderBy: args.orderBy
     };
     return prisma.query.comments(opArgs, info);
   },
